b06cb51e8beba43022de09aea0f18b9b2950d07c
Tim Jones <tjones@dius.com.au>
2017-08-24 12:53:52 +1000

    Use timbash for logging

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+#!/bin/bash -eu
+SCRIPT_DIR=`cd $(dirname "$0"); pwd` # Script can now run from anywhere
+. "$SCRIPT_DIR"/timbash/lib-logging.sh       # Set up logging
 
 function inst {
-  if [ -f ~/.$1 ] ; then
-    DIFF=$(diff ~/.$1 .$1)
+  if [ -f ~/."$1" ] ; then
+    DIFF=$(diff ~/."$1" ."$1")
     if [ "$DIFF" != "" ] ;
     then
-      echo "[x] Skipping $1, because there's a different local version"
-    fi
+      error "Skipping $1, because there's a different local version"
+    else
+      warn "Already installed or otherwise identical: '$1'"
+    fi 
   else 
-    echo "[ ] Installing $1"
-    ln -s `pwd`/.$1 ~/.$1
+    log "Installing $1"
+    ln -s `pwd`/."$1" ~/."$1"
   fi
 }  
 
@@ -17,6 +21,6 @@ inst vimrc
 inst profile
 inst gitignore_global
 
-echo "[ ] Configuring git to always use ssh and use gitignore_global"
-git config --global url."git@github.com:".insteadOf "https://github.com/"
+log "Configuring git to always use ssh and use gitignore_global" 
+git config --global url."git@github.com:".insteadOf "https://github.com/" # This allows private repos in Go, among other things
 git config --global core.excludesfile ~/.gitignore_global
