58d02bf649065b3eb91a694827110f80e2a4f692
Timothy Jones <tjones@dius.com.au>
2018-03-14 15:41:35 +1100

    Update prompt colours

--- master-config/.git-prompt-colors.sh
+++ master-config/.git-prompt-colors.sh
@@ -6,13 +6,13 @@ override_git_prompt_colors() {
   HOST_COLOUR="\[\033[38;5;075m\]"
   USER_COLOUR="\[\033[38;5;033m\]"
   GIT_PROMPT_THEME_NAME="Single_line_nono031"
-  GIT_PROMPT_BRANCH="${Green}"
+  GIT_PROMPT_BRANCH="${Yellow}"
   GIT_PROMPT_PREFIX="("                 # start of the git info string
   GIT_PROMPT_SUFFIX=")"                 # the end of the git info string
   GIT_PROMPT_MASTER_BRANCH="${GIT_PROMPT_BRANCH}"
   GIT_PROMPT_UNTRACKED=" ${Red}…${ResetColor}"
-  GIT_PROMPT_CHANGED="${Yellow}✚ "
-  GIT_PROMPT_STAGED="${Magenta}●"
+  GIT_PROMPT_CHANGED="${Red}✚ "
+  GIT_PROMPT_STAGED="${Green}●"
 
   GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${USER_COLOUR}\u${White}@${HOST_COLOUR}\h${White}:${USER_COLOUR}\w${ResetColor}"
   GIT_PROMPT_START_ROOT="_LAST_COMMAND_INDICATOR_ ${BoldRed}\u${White}@${HOST_COLOUR}\h${White}:${USER_COLOUR}\w${ResetColor}"
