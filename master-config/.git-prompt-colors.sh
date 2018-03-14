76a39e59522ec9c585447fa4937839f3d76b454b
Timothy Jones <tjones@dius.com.au>
2018-03-14 17:50:39 +1100

    Improve formatting of git status line

--- master-config/.git-prompt-colors.sh
+++ master-config/.git-prompt-colors.sh
@@ -6,19 +6,19 @@ override_git_prompt_colors() {
   HOST_COLOUR="\[\033[38;5;075m\]"
   USER_COLOUR="\[\033[38;5;033m\]"
   GIT_PROMPT_THEME_NAME="Single_line_nono031"
-  GIT_PROMPT_BRANCH="${Yellow}"
+  GIT_PROMPT_BRANCH="${BoldWhite}"
   GIT_PROMPT_PREFIX="("                 # start of the git info string
   GIT_PROMPT_SUFFIX=")"                 # the end of the git info string
   GIT_PROMPT_MASTER_BRANCH="${GIT_PROMPT_BRANCH}"
-  GIT_PROMPT_UNTRACKED=" ${Red}…${ResetColor}"
-  GIT_PROMPT_CHANGED="${Red}✚ "
+  GIT_PROMPT_UNTRACKED="${Red}⊘"
+  GIT_PROMPT_CHANGED="${Red}≈"
   GIT_PROMPT_STAGED="${Green}●"
 
   GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${USER_COLOUR}\u${White}@${HOST_COLOUR}\h${White}:${USER_COLOUR}\w${ResetColor}"
   GIT_PROMPT_START_ROOT="_LAST_COMMAND_INDICATOR_ ${BoldRed}\u${White}@${HOST_COLOUR}\h${White}:${USER_COLOUR}\w${ResetColor}"
 
-  GIT_PROMPT_END_USER="${ResetColor} $ "
-  GIT_PROMPT_END_ROOT=" # ${ResetColor}"
+  GIT_PROMPT_END_USER="${ResetColor}$ "
+  GIT_PROMPT_END_ROOT="# ${ResetColor}"
 }
 
 reload_git_prompt_colors "Custom"
